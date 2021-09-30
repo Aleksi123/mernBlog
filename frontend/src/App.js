f8a2a77a2542c6b02d1f3a5dbb9bdc45661e7c20
Aleksi <a12345@live.fi>
2021-09-30 12:11:09 +0300

    Created components
    - PostList, PostItem, PostAdd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import PostList from "./components/PostList";
 import TopBar from "./components/TopBar";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <PostList />
     </div>
   );
 }
